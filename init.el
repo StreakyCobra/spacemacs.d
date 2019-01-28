3a110a04e61afc49a6cb2d4a4b6279ee37ac4e88
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-01-28 15:45:39 +0100

    Add packages from TheBB

--- init.el
+++ init.el
@@ -29,7 +29,7 @@ This function should only modify configuration layer settings."
    ;; If non-nil layers with lazy install support are lazy installed.
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/")
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
@@ -46,6 +46,7 @@ This function should only modify configuration layer settings."
      docker
      emacs-lisp
      emoji
+     encoding
      ess
      git
      github
@@ -57,6 +58,7 @@ This function should only modify configuration layer settings."
      latex
      markdown
      nginx
+     no-dots
      multiple-cursors
      org
      python
