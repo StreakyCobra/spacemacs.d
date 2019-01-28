70807db2a09ab4b23eae89562ae7bd8bbd4ba592
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-01-28 11:47:49 +0100

    Enable default commented layers

--- init.el
+++ init.el
@@ -40,20 +40,20 @@ This function should only modify configuration layer settings."
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      helm
-     ;; auto-completion
-     ;; better-defaults
+     auto-completion
+     better-defaults
      emacs-lisp
-     ;; git
-     ;; markdown
+     git
+     markdown
      multiple-cursors
      treemacs
-     ;; org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
-     ;; version-control
+     org
+     (shell :variables
+            shell-default-height 30
+            shell-default-position 'bottom)
+     spell-checking
+     syntax-checking
+     version-control
      )
 
    ;; List of additional packages that will be installed without being
