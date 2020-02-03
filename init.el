e3ae7084ba9bea24bad1253511a3e32018005b8d
Fabien Dubosson <fabien.dubosson@gmail.com>
2020-02-03 08:44:48 +0100

    Add org-agenda and remember layouts

--- init.el
+++ init.el
@@ -285,7 +285,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; If non-nil then the last auto saved layouts are resumed automatically upon
    ;; start. (default nil)
-   dotspacemacs-auto-resume-layouts nil
+   dotspacemacs-auto-resume-layouts t
 
    ;; If non-nil, auto-generate layout name when creating new layouts. Only has
    ;; effect when using the "jump to layout by number" commands. (default nil)
@@ -525,6 +525,9 @@ before packages are loaded."
    abbrev-file-name (concat user-home-directory ".abbrev_defs")
    ;; lockfiles
    create-lockfiles nil
+   ;; org
+   org-agenda-files '("~/documents/org/"
+                      "~/documents/calendars")
    ;; ranger
    ranger-cleanup-on-disable t
    ranger-dont-show-binary t
